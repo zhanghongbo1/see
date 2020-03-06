8dc2849e960e7cadad74c3415652116349d8d44b
zhanghongbo1 <582074370@qq.com>
2020-03-07 01:23:18 +0800

    主分支

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <label for="dd">
+    <input type="file" id="dd" accept="image/*" style="display:none">
+    <img src="./assets/timg.jpg" alt="">
+    </label>
+  </div>
+</template> 
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+img{
+  width: 100px;
+  height: 100px;
+}
+</style>
