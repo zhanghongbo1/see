897c71c2fc4fbc61be822f6f5167b97cf06c8dba
张先生11 <582074370@qq.com>
2020-03-13 23:05:28 +0800

    13

--- src/123.js
+++ src/123.js
@@ -1,7 +1,3 @@
 const b=0;
 const e=4
-<<<<<<< HEAD
 const a=5
-=======
-const c=5
->>>>>>> remotes/origin/dev-dev
--- src/i.js
+++ src/i.js
@@ -1,9 +1,5 @@
 const b=10
 const c=5
-<<<<<<< HEAD
 const c=6 
 const y=7
 const a=5
-=======
-const a=8
->>>>>>> remotes/origin/dev-dev
